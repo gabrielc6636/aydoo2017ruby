1af57a7c2cee8661207d3174a0b01e989e4ba3fa
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-21 01:22:43 -0300

    test impresor

---
+++ factores-primos-4/spec/impresor_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec' 
+require_relative '../model/ImpresorAscendente'
+require_relative '../model/ImpresorDescendente'
+
+describe 'ImpresorAscendente' do
+
+	before do
+      @impresorAsc = ImpresorAscendente.new
+      @respuestaASC = @impresorAsc.imprimirConFormato('360','ASC')
+
+    end
+
+  describe "OK" do
+    
+    it "si tengo 360 de parametro obtengo 2 2 2 3 3 5" do
+        expect(@respuestaASC).to eq ' 2  2  2  3  3  5 '
+      end
+    end
+  
+end
+
+describe 'ImpresorDescendente' do
+
+	before do
+
+	  @impresorDsc = ImpresorDescendente.new
+      @respuestaDESC = @impresorDsc.imprimirConFormato('360','DESC')
+
+    end
+
+  describe "OK" do
+    
+    it "si tengo 360 de parametro obtengo 5  3  3  2  2  2" do
+        expect(@respuestaDESC).to eq ' 5  3  3  2  2  2 '
+      end 
+    end
+  
+end
