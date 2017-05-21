0a892030040fbce841edb0c46d5c1747bab6f66e
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-21 00:27:29 -0300

    test validador

---
+++ factores-primos-4/spec/validador_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec' 
+require_relative '../model/Validador'
+
+describe 'Validador' do
+
+	before do
+      @validador = Validador.new
+      @valido360 = @validador.es_Numerico('360')
+      @validoGabriel = @validador.es_Numerico('Gabriel')
+
+    end
+
+  describe "OK" do
+    
+    it 'si tengo 360 de parametro es numerico' do
+    expect(@valido360).to eq true
+  end
+    
+    end
+  
+  describe "Bad Request" do
+    
+    it 'si tengo Gabriel de parametro no es numerico' do
+    expect(@validoGabriel).to eq false
+  end
+  
+end
