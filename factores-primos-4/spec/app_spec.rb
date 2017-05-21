0e2839190dc4e0a62d8385d08e05f0e392ead51d
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-21 19:55:53 -0300

    app_spec

---
+++ factores-primos-4/spec/app_spec.rb
@@ -0,0 +1,53 @@
+require_relative 'spec_helper.rb'
+
+describe 'APP' do
+
+	it "llamada a get/primos sin paramtros deberia ser ok" do
+	    get "/primos"
+
+	    expect(last_response.status).to eq 200
+	end
+
+	it "llamada a post/primos sin parametros deberia tener status 200" do
+	    post "/primos"
+
+	    expect(last_response.status).to eq 200
+	end
+
+	it "llamada a post/primos con parametro  12 deberia ser 2,2,3" do
+	    post "/primos", params = {"x" => "12"}
+	    
+	    expect(last_response.body).to eq "2,2,3"
+	end
+
+	it "llamada a get/primos con parametro 12 deberia ser 3,2,2" do
+	    get "/primos", params = {"x" => "12"}
+	    
+	    expect(last_response.body).to eq "3,2,2"
+	end
+
+	it "llamada a post/primos con parametro alfanumerico deberia tener status 400" do
+	    post "/primos", params = {"x" => "12a"}
+	    
+	    expect(last_response.status).to eq 400
+	end
+
+	it "llamada a get/primos con parametro alfanumerico deberia tener status 400" do
+	    get "/primos", params = {"x" => "12a"}
+	    
+	    expect(last_response.status).to eq 400
+	end
+
+	it "llamada a post/primos con parametro 0 deberia ser vacio" do
+	    post "/primos", params = {"x" => "0"}
+	    
+	    expect(last_response.body).to eq ""
+	end
+
+	it "llamada a get/primos con parametro 0 deberia ser vacio" do
+	    get "/primos", params = {"x" => "0"}
+	    
+	    expect(last_response.body).to eq ""
+	end
+
+end
