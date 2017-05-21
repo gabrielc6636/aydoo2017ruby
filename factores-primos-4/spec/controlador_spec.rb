f086934bd6846321a92bf54fc463a66c12351b3c
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-20 23:59:17 -0300

    test controlador

--- factores-primos-4/spec/controlador_spec.rb
+++ factores-primos-4/spec/controlador_spec.rb
@@ -1,26 +1,37 @@
 require 'rspec' 
-require_relative '../model/controlador'
 require_relative '../model/respuesta'
+require_relative '../model/controlador'
 
 describe 'Controlador' do
 
-  let(:controlador) { Controlador.new }  
-   
-  it 'tengo 360 de parametro reviso que sea numerico' do
-    expect(controlador.validarParametro('360')).to eq true
-  end
-  
-  it 'tengo Gabriel de parametro reviso que sea numerico' do
-    expect(controlador.validarParametro('Gabriel')).to eq false
-  end
-  
-  it 'tengo 360 de parametro reviso que sea numerico' do
-    respuesta  = controlador.generarFactores('360')
-    expect(respuesta.getStatus).to eq 200
-  end
-  
-  it 'tengo Gabriel de parametro reviso que sea numerico' do
-    expect(controlador.generarFactores('Gabriel').getStatus).to eq 400
-  end
+	before do
+      @controlador = Controlador.new
+      @valido360 = @controlador.validarParametro('360')
+      @validoGabriel = @controlador.validarParametro('Gabriel')
+      @respuestaOK = @controlador.generarFactores('360')
+	    @respuestaBR = @controlador.generarFactores('Gabriel')
+    end
 
+  describe "OK" do
+    
+    it 'tengo 360 de parametro reviso que sea numerico' do
+    expect(@valido360).to eq true
+  end
+    
+      it "si tuve un valor numerico tengo que tener 200" do
+        expect(@respuestaOK.getStatus).to eq 200
+      end
+    end
+  
+  describe "Bad Request" do
+    
+    it 'tengo Gabriel de parametro reviso que sea numerico' do
+    expect(@validoGabriel).to eq false
+  end
+    
+      it "si tuve un valor no numerico tengo que tener 400" do
+        expect(@respuestaBR.getStatus).to eq 400
+      end
+    end
+  
 end
