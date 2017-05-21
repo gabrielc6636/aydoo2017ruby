26b4b77661b4440a4715502ba6fc82d80bf6643a
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-21 20:04:06 -0300

    app_spec

--- factores-primos-4/spec/app_spec.rb
+++ factores-primos-4/spec/app_spec.rb
@@ -2,37 +2,37 @@ require_relative 'spec_helper.rb'
 
 describe 'APP' do
 
-	it "llamada a get/primos sin paramtros deberia ser ok" do
+	it "si llamo a get/primos sin paramtros no esta OK" do
 	    get "/primos"
 
-	    expect(last_response.status).to eq 200
+	    expect(last_response.status).to eq 400
 	end
 
-	it "llamada a post/primos sin parametros deberia tener status 200" do
+	it "si llamo a post/primos sin parametros no esta OK" do
 	    post "/primos"
 
-	    expect(last_response.status).to eq 200
+	    expect(last_response.status).to eq 400
 	end
 
-	it "llamada a post/primos con parametro  12 deberia ser 2,2,3" do
+	it "si llamo a post/primos con parametro  12 deberia ser 3,2,2" do
 	    post "/primos", params = {"x" => "12"}
-	    
-	    expect(last_response.body).to eq "2,2,3"
-	end
-
-	it "llamada a get/primos con parametro 12 deberia ser 3,2,2" do
-	    get "/primos", params = {"x" => "12"}
 	    
 	    expect(last_response.body).to eq "3,2,2"
 	end
 
-	it "llamada a post/primos con parametro alfanumerico deberia tener status 400" do
+	it "si llamo a get/primos con parametro 12 deberia ser 2,3,3" do
+	    get "/primos", params = {"x" => "12"}
+	    
+	    expect(last_response.body).to eq "2,2,3"
+	end
+
+	it "si llamo a post/primos con parametro alfanumerico deberia tener status 400" do
 	    post "/primos", params = {"x" => "12a"}
 	    
 	    expect(last_response.status).to eq 400
 	end
 
-	it "llamada a get/primos con parametro alfanumerico deberia tener status 400" do
+	it "si llamo a get/primos con parametro alfanumerico deberia tener status 400" do
 	    get "/primos", params = {"x" => "12a"}
 	    
 	    expect(last_response.status).to eq 400
