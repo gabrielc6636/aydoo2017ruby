ffab92808867e1d0ecdd6dc0b35324b4ea7ef696
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-21 00:33:40 -0300

    test factorizador

---
+++ factores-primos-4/spec/factorizador_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec' 
+require_relative '../model/factorizador'
+
+describe 'Factorizador' do
+
+	before do
+      @factorizador = Factorizador.new
+      @valido360 = @factorizador.calcularFactores('360')
+	  @valido1 = @factorizador.calcularFactores('1')
+
+    end
+
+  describe "OK" do
+    
+    it 'si tengo 360 de parametro obtengo 2 2 2 3 3 5' do
+    expect(@valido360.length).to eq 6
+  end
+    
+    end
+  
+  describe "Bad Request" do
+    
+    it 'si tengo 360 de parametro obtengo nada' do
+    expect(@valido1.length).to eq 0
+  end
+  
+end
