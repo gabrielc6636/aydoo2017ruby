447d24ee979a47b694f1d639b31449667c0b8724
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-20 23:51:09 -0300

    test respuesta

--- factores-primos-4/spec/respuesta_spec.rb
+++ factores-primos-4/spec/respuesta_spec.rb
@@ -14,4 +14,10 @@ describe 'Respuesta' do
       end
     end
   
+  describe "Bad Request" do
+      it "si tuve un valor no numerico tengo que tener 400" do
+        expect(@respuestaBR.getStatus).to eq 400
+      end
+    end
+  
 end
