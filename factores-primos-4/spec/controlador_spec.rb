919d6dc23095465ef88b3677afc39139c0f7e20c
gabrielc6636 <gabrielc6636@gmail.com>
2017-05-21 00:06:10 -0300

    test controlador

--- factores-primos-4/spec/controlador_spec.rb
+++ factores-primos-4/spec/controlador_spec.rb
@@ -8,30 +8,30 @@ describe 'Controlador' do
       @controlador = Controlador.new
       @valido360 = @controlador.validarParametro('360')
       @validoGabriel = @controlador.validarParametro('Gabriel')
-      @respuestaOK = @controlador.generarFactores('360')
-	    @respuestaBR = @controlador.generarFactores('Gabriel')
+      #@respuestaOK = @controlador.generarFactores('360')
+	    #@respuestaBR = @controlador.generarFactores('Gabriel')
     end
 
   describe "OK" do
     
-    it 'tengo 360 de parametro reviso que sea numerico' do
+    it 'si tengo 360 de parametro es numerico' do
     expect(@valido360).to eq true
   end
     
-      it "si tuve un valor numerico tengo que tener 200" do
-        expect(@respuestaOK.getStatus).to eq 200
-      end
+      #it "si tuve un valor numerico tengo que tener 200" do
+      #  expect(@respuestaOK.getStatus).to eq 200
+      #end
     end
   
   describe "Bad Request" do
     
-    it 'tengo Gabriel de parametro reviso que sea numerico' do
+    it 'si tengo Gabriel de parametro no es numerico' do
     expect(@validoGabriel).to eq false
   end
     
-      it "si tuve un valor no numerico tengo que tener 400" do
-        expect(@respuestaBR.getStatus).to eq 400
-      end
+      #it "si tuve un valor no numerico tengo que tener 400" do
+      #  expect(@respuestaBR.getStatus).to eq 400
+      #end
     end
   
 end
